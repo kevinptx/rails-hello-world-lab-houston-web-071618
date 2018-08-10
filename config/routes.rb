e6b660e1513b92c8383796f5d678b81a31494a71
kevinptx <kevin.peery@gmail.com>
2018-08-10 12:52:15 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get "hello_world/hello_world"
+
+  get "/hello_world", to: "hello_world#hello_world"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
